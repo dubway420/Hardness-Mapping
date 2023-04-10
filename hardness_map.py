2d3b8eb31e0f0c33b62c81ab74e9de8c41056463
Huw Rhys Jones <43721202+dubway420@users.noreply.github.com>
2023-04-10 01:02:42 +0100

    Save functionality and validation complete. Removed test file.

--- hardness_map.py
+++ hardness_map.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+from openpyxl import Workbook
+import os
 
 DEFAULT_X_COLUMN_INT = 5
 DEFAULT_Y_COLUMN_INT = 6
@@ -11,7 +13,29 @@ class HardnessMap:
 
         self.input_filename = input_filename
 
-    def get_data(self):    
+        # Check that the file exists 
+        # ===================================================
+        if not os.path.isfile(self.input_filename):
+            print(f"\nWarning: a file with the filename you specified ({self.input_filename}) can not be found.\n")
+
+
+        # Split the filename into the name and extension
+        # ===================================================
+        filename_split = os.path.splitext(self.input_filename)
+
+        self.filename_noext = filename_split[0]
+        self.file_ext = filename_split[1]
+
+        # Check that the extension is .csv
+        if not self.file_ext == ".csv":
+            print(f"\nWarning: the filename you specified ({self.input_filename}) is not a .csv file.\n")
+
+
+    def get_data(self, encoding='utf-16', delineator='\t', header_row=0):    
+
+        self.encoding = encoding
+        self.delineator = delineator
+        self.header_row = header_row
 
         # Extracts data from the worksheet
         self.dataframe = pd.read_csv(self.input_filename, encoding='utf-16', sep='\t', header=0)
@@ -20,7 +44,7 @@ class HardnessMap:
         self.keys = self.dataframe.keys()
 
         # Ensure that the default columns are valid - if not this class method will reassign them or throw an error
-        assert(self.default_column_verification())
+        assert(self.__default_column_verification__())
 
         # Extract the x and y columns
         self.x = self.dataframe[self.keys[self.x_column]].values
@@ -42,7 +66,7 @@ class HardnessMap:
 
         
 
-    def default_column_verification(self):
+    def __default_column_verification__(self):
         
         self.x_column = DEFAULT_X_COLUMN_INT
         self.y_column = DEFAULT_Y_COLUMN_INT
@@ -130,3 +154,62 @@ class HardnessMap:
                 # Assign these values to the hardness map
                 self.hardness_map[i, j] = hardness_value
 
+
+    def save_to_excel(self, save_original_data=True, save_filename="", extension=".xlsx"):
+
+        self.save_extension = extension
+        self.__save_extension_validation__()
+
+        # If the user does not specify a save filename, use the input filename
+        if save_filename == "":
+            self.save_filename = self.filename_noext + self.save_extension
+        # Else use the user specified name    
+        else:
+            self.save_filename = save_filename + self.save_extension
+
+        # Instantiate the Excel workbook
+        wb = Workbook()
+        ws = wb.active   
+
+        ws.title = "Map"
+
+        # append the x values to the top row of the excel sheet
+        first_row_list = self.y_unique.tolist()
+
+        # place a blank cell top left
+        first_row_list.insert(0, " ")
+
+        # Write the first row to the sheet
+        ws.append(first_row_list)
+
+        # Write each row of the hardness map to the Excel sheet
+        for x_inc, row in zip(self.x_unique, self.hardness_map):
+
+            row_list = row.tolist()
+
+            # Put the x increment in the left most column
+            row_list.insert(0, x_inc)
+            ws.append(row_list)
+
+
+        if save_original_data:
+            ws2 = wb.create_sheet("Original")
+
+            original_data = self.dataframe.values
+
+            ws2.append(self.keys.to_list())
+            for row in original_data:
+                ws2.append(row.tolist())
+
+        # Save the sheet
+        wb.save(self.save_filename)
+
+    def __save_extension_validation__(self):
+
+        # If the user specifies an extension that does not have a fullstop at the start, add one
+        if self.save_extension[0] != ".":
+            self.save_extension = "." + self.save_extension
+
+        if len(self.save_extension) < 3 or len(self.save_extension.split(".")) > 2:
+            print(f"\nWarning: the extension you specified for saving ({self.save_extension}) may not be valid. Consider review.\n")
+    
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from hardness_map import HardnessMap
 hardness_map = HardnessMap("CCT_PM_0.1Cs-1.csv")
 hardness_map.get_data()
 hardness_map.get_hardness_map()
+hardness_map.save_to_excel(save_filename="hello", extension="xl.sx")
 
-print(hardness_map.hardness_map)
+# print(hardness_map.hardness_map)
 

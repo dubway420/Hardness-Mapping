3a1c2dedba732a696705555c493ef3217e7941d0
Huw Rhys Jones <43721202+dubway420@users.noreply.github.com>
2023-04-16 04:08:35 +0100

    minor change - path simplified

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from hardness_map import multiple_hardness_maps
 
 
-multiple_hardness_maps(r"C:\Users\janus\OneDrive\Documents\HardnessMapping\inputs", "Outputs", save_excel=True, save_image=True, axis_labels=True, vrange=[100, 700])
+multiple_hardness_maps(r"inputs", "Outputs", save_excel=True, save_image=True, axis_labels=True, vrange=[100, 700])
 
